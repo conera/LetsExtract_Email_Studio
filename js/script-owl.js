6e297f5bfd585b61d58fd89d2f2449b5ea15ee81
Kirill.info <kiril.net96@gmail.com>
2022-07-14 18:05:07 +0200

    Initial commit

---
+++ js/script-owl.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+            items: 2,
+        margin:30,
+        loop: true,
+        nav:true,
+        navText: ["<img src='img/arrow-left.svg'>","<img src='img/arrow-right.svg'>" ],
+
+    }
+
+    );
+});
