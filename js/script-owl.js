d2495144cc78d198c227a7c9274560efa61bd65b
Kirill.info <kiril.net96@gmail.com>
2022-07-15 12:03:59 +0200

    added responsive, fixed btn-links hovers

--- js/script-owl.js
+++ js/script-owl.js
@@ -6,6 +6,18 @@ $(document).ready(function(){
         nav:true,
         navText: ["<img src='img/arrow-left.svg'>","<img src='img/arrow-right.svg'>" ],
 
+        responsiveClass:true,
+            responsive : {
+                // breakpoint from 0 up
+                320 : {
+                    items : 1,
+                    margin: 0
+                },
+                768 : {
+                    items : 2,
+                    margin: 30
+                }
+            }
     }
 
     );
